5f40483770efb14c6bb519fe712e48d859107fdf
CamiloRaulBaezaHenriquez <camilorbh@hotmail.com>
2024-06-24 20:56:58 -0400

    test s3 per

--- s3.tf
+++ s3.tf
@@ -2,10 +2,10 @@ resource "aws_s3_bucket" "s3_bucket_n1" {
     bucket = local.s3_bucket_n1
 }
 
-# resource "aws_s3_bucket" "s3_bucket_n2" {
-#   bucket = local.s3_bucket_n2
-#   acl = "public-read"
-#   tags = {
-#     Name        = "My bucket"
-#   }
-# }
+resource "aws_s3_bucket" "s3_bucket_n2" {
+  bucket = local.s3_bucket_n2
+  acl = "public-read"
+  tags = {
+    Name        = "My bucket"
+  }
+}
