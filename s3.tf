2fe38d6ea6040fc7c2bd512fd61e7c1b1a777085
CamiloRaulBaezaHenriquez <camilorbh@hotmail.com>
2024-06-25 00:13:26 -0400

    test s3 ACL

--- s3.tf
+++ s3.tf
@@ -35,10 +35,10 @@ resource "aws_s3_bucket_public_access_block" "s3_bucket_n2" {
   restrict_public_buckets = true
 }
 
-# resource "aws_s3_object" "s3_object_tarro_files" {
-#   for_each = fileset("./Studio-develop/","**")
-#   bucket = aws_s3_bucket.s3_bucket_n2.id
-#   key = "Studio-develop/${each.value}"
-#   source = "./Studio-develop/${each.value}"
-#   etag = filemd5("./Studio-develop/${each.value}")
-# }
+resource "aws_s3_object" "s3_object_tarro_files" {
+  for_each = fileset("./Studio-develop/","**")
+  bucket = aws_s3_bucket.s3_bucket_n2.id
+  key = "Studio-develop/${each.value}"
+  source = "./Studio-develop/${each.value}"
+  etag = filemd5("./Studio-develop/${each.value}")
+}
