dc44d20d289f3156d2d96f614849b00ed9d32691
Camilo Baeza Henríquez <camilorbh@hotmail.com>
2025-03-26 03:08:36 -0300

    rey dau

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
-# ## Buckets
-# resource "aws_s3_bucket" "s3_bucket_n1" {
-#     bucket = local.s3_bucket_n1
-# }
+## Buckets
+resource "aws_s3_bucket" "s3_bucket_n1" {
+    bucket = local.s3_bucket_n1
+}
 
 # resource "aws_s3_bucket" "s3_bucket_n2" {
 #   bucket = local.s3_bucket_n2
