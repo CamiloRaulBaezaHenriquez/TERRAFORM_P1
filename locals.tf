3b080832d108afaaa9832522c218ee2224f32fca
CamiloRaulBaezaHenriquez <camilorbh@hotmail.com>
2025-04-09 15:02:10 +0000

    rasengan

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
     s3_bucket_n1 = "tarron1-cbaeza-${terraform.workspace}-reydau"
-    s3_bucket_n2 = "bc-cbaeza-${terraform.workspace}-AWSInnovate"
+    s3_bucket_n2 = "bucket-cbaeza-${terraform.workspace}-awsinnovate"
 }
